95335c5035677d79c62b443a969bce3353f73946
Huy Pham <phamgiahuy9h@gmail.com>
2024-12-20 15:04:45 +0700

    change 1

--- config/routing.js
+++ config/routing.js
@@ -1,13 +1,10 @@
-const EmployeeKPIController = require("../routers/employeesRouter") 
-const KPIController = require("../routers/doanhsoRouter")
+
 const SubmitRouter = require("../routers/submitRouter")
-const AuthenticateRouter = require("../routers/Authenticate")
+
 const setupRouting = (app) => {
   console.log("Setting up routes.");
-  app.use("/api/v1/employees", EmployeeKPIController); // Thiết lập route cho nhân viên
-  app.use("/api/v1/doanhso", KPIController);
   app.use("/api/v1/submit", SubmitRouter);
-  app.use("/api/v1/auth", AuthenticateRouter);
+
 };
 
 module.exports = setupRouting;
--- routers/Authenticate.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authenticateController = require("../controllers/Authenticate_Controller");
-router.post("/", authenticateController.index);
-
-module.exports = router;
